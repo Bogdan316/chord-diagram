ec65b64dfc8efb2c43cfc95c74392f355056ab7d
BOGDAN <bogdan.pirtica@student.upt.ro>
2023-06-10 17:31:37 +0300

    asd

--- chart.js
+++ chart.js
@@ -1,78 +1,96 @@
 import * as d3 from "d3"
 
-const hierarchy = [
-    { 'name': "ClassA", "parent": "" },
-    { 'name': "ClassB", "parent": "ClassA" },
-    { 'name': "ClassC", "parent": "ClassA" },
-]
+const RADIUS = 400;
 
-var svg = d3.select("#chart")
+const adjacencyMatrix = [
+    [0, 0, 0, 5, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 0, 0, 0, 0, 3, 0, 0],
+    [5, 0, 0, 0, 0, 0, 7, 0],
+    [0, 0, 0, 0, 0, 0, 0, 4],
+    [0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 2, 0, 7, 0, 0, 0, 0],
+    [0, 0, 0, 0, 4, 0, 0, 0],
+];
+
+
+const adjacencyList = {
+    "ClassAAA": ["ClassBAA"],
+    "ClassABA": ["ClassCA"],
+    "ClassABB": ["ClassBCA"],
+    "ClassBAA": ["ClassAAA", "ClassCA"],
+    "ClassBBA": ["ClassCB"],
+    "ClassBCA": ["ClassABB"],
+    "ClassCA": ["ClassABA", "ClassBAA"],
+    "ClassCB": ["ClassBBA"]
+}
+
+const classHierarchyJson =
+    [
+        { "name": "Object", "parent": "" },
+
+        { "name": "ClassA", "parent": "Object" },
+        { "name": "ClassAA", "parent": "ClassA" },
+        { "name": "ClassAB", "parent": "ClassA" },
+        { "name": "ClassAAA", "parent": "ClassAA" },
+        { "name": "ClassABA", "parent": "ClassAB" },
+        { "name": "ClassABB", "parent": "ClassAB" },
+
+        { "name": "ClassB", "parent": "Object" },
+        { "name": "ClassBA", "parent": "ClassB" },
+        { "name": "ClassBB", "parent": "ClassB" },
+        { "name": "ClassBC", "parent": "ClassB" },
+        { "name": "ClassBAA", "parent": "ClassBA" },
+        { "name": "ClassBBA", "parent": "ClassBB" },
+        { "name": "ClassBCA", "parent": "ClassBC" },
+
+        { "name": "ClassC", "parent": "Object" },
+        { "name": "ClassCA", "parent": "ClassC" },
+        { "name": "ClassCB", "parent": "ClassC" }
+    ];
+
+var classHierarchy = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent)
+    (classHierarchyJson);
+
+const leaves = classHierarchy.leaves();
+leaves.forEach((l, idx) => l.idx = idx);
+
+
+const svg = d3.select("#chart")
     .append("svg")
     .attr("width", window.innerWidth)
     .attr("height", window.innerHeight)
     .append("g")
     .attr("transform", `translate(${window.innerWidth / 2}, ${window.innerHeight / 2})`)
 
-
-var matrix = [
-    [11975, 5871, 8916, 2868],
-    [1951, 10048, 2060, 6171],
-    [8010, 16145, 8090, 8045],
-    [1013, 990, 940, 6907]
-];
-
-var chord = d3.chord()
-    .sortSubgroups(d3.ascending)
-    // .padAngle(0.1)
-    (matrix)
-
-// svg.datum(chord)
-//     .append("g")
-//     .selectAll("path")
-//     .data((d) => d)
-//     .enter()
-//     .append("path")
-//     .style("fill", "cornflowerblue")
-//     .style("stroke", "black")
-//     .attr("d", d3.ribbon().radius(400))
-//     .append("title")
-//     .text(d => `${d.source.value} → ${d.target.value}`)
+const chord = d3.chord()
+    // .sortSubgroups(d3.ascending)
+    (adjacencyMatrix)
 
 
-const treeData = [
-    [
-        { "name": "10", "parent": "" },
+/* DRAW ARCS */
 
-        { "name": "5", "parent": "10" },
-        { "name": "6", "parent": "5" },
-        { "name": "4", "parent": "5" },
-        { "name": "0", "parent": "6" },
-        { "name": "1", "parent": "6" },
-        { "name": "2", "parent": "4" },
+const updateAngles = function (groups) {
+    // update arc angles starting from the leaves (most inner arcs of the diagram)
+    // to parents (classes that are higher in the hierarchy)
 
-        { "name": "7", "parent": "10" },
-        { "name": "8", "parent": "7" },
-        { "name": "9", "parent": "8" },
-        { "name": "3", "parent": "9" },
-    ],
-];
-
-var randomColor = () => d3.rgb(...hsvToRgb(Math.random(), 0.3, 0.5));
-
-const updateAngles = function (root, groups) {
-    for (const leaf of root.leaves()) {
-        const group = groups[leaf.id];
+    for (const leaf of leaves) {
+        const group = groups[leaf.idx];
 
         leaf.startAngle = group.startAngle;
         leaf.endAngle = group.endAngle;
     }
 
-    var currentLevel = new Set(root.leaves().map(d => d.parent));
+    // go through the hierarchy level by level
+    var currentLevel = new Set(leaves.map(d => d.parent));
 
     while (currentLevel.size !== 0) {
         const nextLevel = new Set();
 
         for (const node of currentLevel) {
+            // calculate the angle of the current arc (correspondig to the current node)
             node.startAngle = d3.min(node.children, c => c.startAngle);
             node.endAngle = d3.max(node.children, c => c.endAngle);
 
@@ -85,35 +103,36 @@ const updateAngles = function (root, groups) {
     }
 }
 
-const bfs = function (tree, groups) {
+const arcsColorScale = d3.scaleSequential()
+    .domain([0, classHierarchy.children.length - 1])
+    .interpolator(d3.interpolateCool)
 
-    updateAngles(tree, groups);
+const drawArcs = function (tree, chord, color) {
+    // use bfs to draw arcs for the provided hierarchy
+    updateAngles(chord.groups);
 
     let q = [];
     tree.visited = true;
     q.push(tree)
 
-    let color = randomColor();
-    tree.color = color;
+    tree.color = d3.rgb(color);
 
-    let brightenAmount = (0.85 - 0.2) / tree.height;
-    console.log(tree.height);
+    const brightenAmount = (0.6 - 0.1) / tree.height;
 
     while (q.length !== 0) {
         const node = q.shift();
 
-
         svg.datum(chord)
             .append("g")
             .selectAll("g")
-            .data(groups)
+            .data(chord.groups)
             .enter()
             .append("g").append("path")
             .style("fill", node.color)
             .style("stroke", "white")
             .attr("d", d3.arc()
-                .innerRadius(400 + node.height * 15)
-                .outerRadius(400 + (node.height + 1) * 15)
+                .innerRadius(RADIUS + node.height * 15)
+                .outerRadius(RADIUS + (node.height + 1) * 15)
                 .startAngle(node.startAngle)
                 .endAngle(node.endAngle));
 
@@ -131,63 +150,54 @@ const bfs = function (tree, groups) {
     }
 }
 
-var root = d3.stratify()
-    .id(d => d.name)
-    .parentId(d => d.parent)
-    (treeData[0]);
-bfs(root.children[0], chord.groups);
+classHierarchy.children.forEach((c, idx) => drawArcs(c, chord, arcsColorScale(idx)))
 
-bfs(root.children[1], chord.groups);
+/* DRAW EDGES */
 
-var cluster = d3.cluster()
-    .size([360, 400]);
+const cluster = d3.cluster()
+    .size([360, RADIUS]);
 
-cluster(root);
+cluster(classHierarchy);
 
-root.leaves().forEach(
+leaves.forEach(
     (leaf, idx) => {
         const group = chord.groups[idx];
         leaf.x = ((group.startAngle + group.endAngle) / 2) * (180 / Math.PI);
     }
 )
 
-var bubble = svg.append("g").selectAll("g");
+leaves.forEach(l => l.data.imports = adjacencyList[l.id]);
 
-
-// bubble = bubble
-//     .data(root.leaves())
-//     .enter().append("circle")
-//     .attr("class", "bubble")
-//     .attr("transform", function (d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 10) + ",0)" })
-//     .attr('r', d => 20)
-//     .attr('stroke', 'black')
-//     .attr('fill', 'red')
-//     .style('opacity', .2)
-
-root.leaves().forEach(l => l.data.imports = ["0", "1", "2", "3"]);
-
-var line = d3.lineRadial()
+const line = d3.lineRadial()
     .curve(d3.curveBundle.beta(0.85))
     .radius(function (d) { return d.y; })
     .angle(function (d) { return d.x / 180 * Math.PI; });
 
-let link = svg.append("g").selectAll("g");
+const weightExtend = d3.extent(adjacencyMatrix.flatMap(r => r));
 
-link = link
-    .data(packageImports(root.leaves()))
-    .enter().append("path")
-    .each(function (d) { 
-        // d[0].x -= 10;
-        d.source = d[0]; 
-        d.target = d[d.length - 1]; 
-        console.log(d[0], d[d.length - 1]);
+const linksColorScale = d3.scaleSequential()
+    .domain(weightExtend)
+    .interpolator(d3.interpolateReds)
+
+const linksWidthScale = d3.scaleLinear()
+    .domain(weightExtend)
+    .range([1, 5]);
+
+const link = svg.append("g").selectAll("g")
+    .data(constructClassLinks(leaves))
+    .enter()
+    .append("path")
+    .each(function (d) {
+        d.source = d[0];
+        d.target = d[d.length - 1];
     })
     .attr("d", line)
     .attr("fill", "none")
-    .style("stroke", "cornflowerblue");
+    .style("stroke",  d => linksColorScale(adjacencyMatrix[d.source.idx][d.target.idx]))
+    .style("stroke-width", d => linksWidthScale(adjacencyMatrix[d.source.idx][d.target.idx]));
 
 // Return a list of imports for the given array of nodes.
-function packageImports(nodes) {
+function constructClassLinks(nodes) {
     var map = {},
         imports = [];
 
@@ -206,44 +216,60 @@ function packageImports(nodes) {
     return imports;
 }
 
+
+// var bubble = svg.append("g").selectAll("g");
+
+// bubble = bubble
+//     .data(leaves)
+//     .enter().append("circle")
+//     .attr("class", "bubble")
+//     .attr("transform", function (d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 10) + ",0)" })
+//     .attr('r', d => 20)
+//     .attr('stroke', 'black')
+//     .attr('fill', 'red')
+//     .style('opacity', .2)
+
 // gradient
 // linii, vazut valoarea (importanta)
 
 
 // colors
 // TODO: generate 
-function hsvToRgb(h, s, v) {
-    let r, g, b;
 
-    let h_i = Math.floor(h * 6);
-    let f = h * 6 - h_i;
-    let p = v * (1 - s);
-    let q = v * (1 - f * s);
-    let t = v * (1 - (1 - f) * s);
+// var randomColor = () => d3.rgb(...hsvToRgb(Math.random(), 0.3, 0.5));
 
-    if (h_i === 0) {
-        [r, g, b] = [v, t, p];
-    }
+// function hsvToRgb(h, s, v) {
+//     let r, g, b;
 
-    if (h_i === 1) {
-        [r, g, b] = [q, v, p];
-    }
+//     let h_i = Math.floor(h * 6);
+//     let f = h * 6 - h_i;
+//     let p = v * (1 - s);
+//     let q = v * (1 - f * s);
+//     let t = v * (1 - (1 - f) * s);
 
-    if (h_i === 2) {
-        [r, g, b] = [p, v, t];
-    }
+//     if (h_i === 0) {
+//         [r, g, b] = [v, t, p];
+//     }
 
-    if (h_i === 3) {
-        [r, g, b] = [p, q, v];
-    }
+//     if (h_i === 1) {
+//         [r, g, b] = [q, v, p];
+//     }
 
-    if (h_i === 4) {
-        [r, g, b] = [t, p, v];
-    }
+//     if (h_i === 2) {
+//         [r, g, b] = [p, v, t];
+//     }
 
-    if (h_i === 5) {
-        [r, g, b] = [v, p, q];
-    }
+//     if (h_i === 3) {
+//         [r, g, b] = [p, q, v];
+//     }
 
-    return [r * 256, g * 256, b * 256].map(Math.floor);
-}
+//     if (h_i === 4) {
+//         [r, g, b] = [t, p, v];
+//     }
+
+//     if (h_i === 5) {
+//         [r, g, b] = [v, p, q];
+//     }
+
+//     return [r * 256, g * 256, b * 256].map(Math.floor);
+// }
